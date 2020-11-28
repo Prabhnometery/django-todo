6932cdc24f9e393d864b9ef4a55c85fc5416f734
Prabh Simran Singh Badwal <61562651+Prabhnometery@users.noreply.github.com>
2020-11-28 14:21:45 -0500

    Added complete authentication including Login, Logout

--- todowoo/urls.py
+++ todowoo/urls.py
@@ -22,8 +22,12 @@ urlpatterns = [
 
     # AUTH
     path('signup/', views.signupuser, name='signupuser'),
+    path('logout/', views.logoutuser, name='logoutuser'),
+    path('login/', views.loginuser, name='loginuser'),
+
 
 
     # TODOS
+    path('', views.home, name='home'),
     path('current/', views.currenttodos, name='currenttodos'),
 ]
