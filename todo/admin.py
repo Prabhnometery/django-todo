0ace3960613ecf5140b8cc9bc2fde599b72e7904
Prabh Simran Singh Badwal <61562651+Prabhnometery@users.noreply.github.com>
2020-11-29 01:43:39 -0500

    Created a Todo Model and worked on the databases

--- todo/admin.py
+++ todo/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Todo
 
-# Register your models here.
+# This will allow us to view the entires which are restricted and are not able to be edited
+class TodoAdmin(admin.ModelAdmin):
+    readonly_fields = ('created',)
+
+admin.site.register(Todo, TodoAdmin)
